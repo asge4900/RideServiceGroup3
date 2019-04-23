20d03d15d786d0371eedc04f907ed2ba473c5ddd
Asger Bo Lassen <asge4900@edu.campusvejle.dk>
2019-04-23 11:21:31 +0200

    You can see all rides

---
+++ RideServiceGroup3.Entities/RideCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RideServiceGroup3.Entities
+{
+    public class RideCategory
+    {
+        public RideCategory(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ RideServiceGroup3.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RideServiceGroup3.Dal;
+using RideServiceGroup3.Entities;
+
+namespace RideServiceGroup3.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        
+        public List<Ride> Rides { get; set; }
+
+        public void OnGet()
+        {
+            RideRepository rideRepo = new RideRepository();
+
+            Rides = rideRepo.GetAllRides();
+        }
+    }
+}
